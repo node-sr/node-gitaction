b096f8c7b72bb138deb872959edcf8611b8677a4
tajindersr <tejinder.upwork@gmail.com>
2021-06-02 00:34:22 +0530

    updated response

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3100;
 app.get('/',(req,res)=>{
-    res.send({msg:'Welcome to express app'})
+    res.send("<h1>Welcome to express app</h1>")
 })
 app.listen(port,()=>{
     console.log(`App running on ${port}`);
