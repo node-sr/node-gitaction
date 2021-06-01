58590cae9f565701306bd02546872d5dca582f89
tajindersr <tejinder.upwork@gmail.com>
2021-06-01 17:19:37 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = 3100;
+app.get('/',(req,res)=>{
+    res.json({msg:'Welcome to express app'})
+})
+app.listen(port,()=>{
+    console.log(`App running on ${port}`);
+})
