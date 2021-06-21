45921b9a9824cc17d534ca6fe370bd5a13b85f07
node-sr <komal.kumar@softradix.in>
2021-06-21 16:00:35 +0530

    update workflow

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const port = 3100;
 app.get('/',(req,res)=>{
     res.send("<h1>Welcome to github actions nodeapp</h1>")
 })
+app.get('/dashboard',(req,res)=>{
+    res.send("<h1>Dashboard</h1>")
+})
 app.listen(port,()=>{
     console.log(`App running on ${port}`);
 })
